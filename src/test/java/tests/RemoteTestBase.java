8d7488fc1017d54396f0846e7650ceeaa6be490e
Kuznetsov_Evgeny <kev35.vol@gmail.com>
2025-11-22 13:34:47 +0300

    Jenkins with params

---
+++ src/test/java/tests/RemoteTestBase.java
@@ -0,0 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class RemoteTestBase {
+    @BeforeEach
+    void setEnvironment(){
+        Configuration.baseUrl = System.getProperty("baseUrl","https://demoqa.com");
+        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion","128.0");
+        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
+        Configuration.remote = System.getProperty("remoteSelenoidUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.timeout = 10000;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo", true));
+
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("allure",new AllureSelenide());
+    }
+    @AfterEach
+    void addAttachments() {
+
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        Selenide.closeWebDriver();
+    }
+}
