dbfe05a2cbef68912d64ee3a79e6ddf91df9f7f6
Kuznetsov_Evgeny <kev35.vol@gmail.com>
2025-11-25 22:59:53 +0300

    TelegramBot commit

--- src/test/java/tests/TelegramBotTests.java
+++ src/test/java/tests/TelegramBotTests.java
@@ -1,13 +1,7 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import java.util.Map;
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -15,23 +9,6 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class TelegramBotTests extends RemoteTestBase {
-
-    @BeforeEach
-    void setEnvironment(){
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.timeout = 10000;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.of(
-                "enableVNC", true,
-                "enableVideo", true));
-
-        Configuration.browserCapabilities = capabilities;
-
-        SelenideLogger.addListener("allure",new AllureSelenide());
-    }
 
     @Test
     @Tag("TelegramBot")
