896eab07a8bae5222fb4859a9a14e4f19c335290
Kuznetsov_Evgeny <kev35.vol@gmail.com>
2025-11-25 23:06:26 +0300

    TelegramBot commit

--- src/test/java/tests/TelegramBotTests.java
+++ src/test/java/tests/TelegramBotTests.java
@@ -1,14 +1,23 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.util.Map;
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class TelegramBotTests extends RemoteTestBase {
+public class TelegramBotTests extends RemoteTestBase{
 
     @Test
     @Tag("TelegramBot")
